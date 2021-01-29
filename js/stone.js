7f065b40c2a53256c4aa6a768efb3936c08062cf
RAJVARDHAN123 <75675172+RAJVARDHAN123@users.noreply.github.com>
2021-01-29 20:48:35 +0530

    Add files via upload

---
+++ js/stone.js
@@ -0,0 +1,33 @@
+class Stone{
+
+    constructor(x,y, r){
+
+     var options = {
+
+      isStatic: false,
+      restitution: 0,
+      friction: 1,
+      denstity: 1.2
+
+     }
+
+     this.body = Bodies.circle(x,y,r);
+     this.r = r;
+
+       World.add(world, this.body);
+
+     this.image = loadImage("images/stone.png");
+
+    }
+
+    display(){
+
+        var pos = this.body.position;
+
+    ellipseMode(RADIUS);
+    imageMode(CENTER);
+    image(this.image, pos.x, pos.y, this.r*2, this.r*2);
+
+    }
+
+}
